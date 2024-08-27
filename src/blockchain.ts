8e631eb2f182e4832c28fdcf6af7e8db8dca2fb7
alecron <alecronster@gmail.com>
2024-08-27 23:43:20 +0200

    [Chapter 2] Add proof-of-work

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,6 +1,6 @@
 import * as CryptoJS from 'crypto-js';
 import { broadcastLatest } from './p2p';
-
+import { hexToBinary } from './utils';
 
 // This first version is almost a copy of the tutorial, but I'm adding some comments and some functions that are not explained in the tutorial
 //      TODO: Improve the code organization and add more comments
@@ -11,18 +11,27 @@ class Block {
     public hash: string;
     public previousHash: string;
 
-    constructor(index: number, data: string, timestamp: number, hash: string, previousHash: string) {
+    // Chapter 2 - Proof of Work
+    public difficulty: number;  // This is the number of zeros that the hash must start with
+    public nonce: number;    // This is the number that will be modified to find the correct hash
+    // “Mining” is basically just trying a different nonce until the block hash matches the difficulty
+
+    constructor(index: number, data: string, timestamp: number, hash: string, previousHash: string,
+        difficulty: number, nonce: number
+    ) {
         this.index = index;
         this.data = data;
         this.timestamp = timestamp;
         this.hash = hash;
         this.previousHash = previousHash;
+        this.difficulty = difficulty;
+        this.nonce = nonce;
     }
 
 }
 
 // MAIN VARIABLES -- BLOCKCHAIN DEFINITION
-const genesisBlock: Block = new Block(0, "Genesis Block", 1465154705, "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7", '');
+const genesisBlock: Block = new Block(0, "Genesis Block", 1465154705, "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7", '', 0, 0);
 
 let blockchain: Block[] = [genesisBlock];
 
@@ -33,21 +42,24 @@ const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 const sockets: WebSocket[] = [];
 
 const generateNextBlock = (blockData: string): Block => {
-    // Note: The getLatestBlock function is not directly implemented in the tutorial
     const previousBlock: Block = getLatestBlock();
+    const difficulty: number = getDifficulty(getBlockchain());
+
     const nextIndex: number = previousBlock.index + 1;
-    const nextTimestamp: number = new Date().getTime() / 1000;
-    const nextHash: string = calculateHash(nextIndex, blockData, nextTimestamp, previousBlock.hash);
-    return new Block(nextIndex, blockData, nextTimestamp, nextHash, previousBlock.hash);
+    const nextTimestamp: number = getCurrentTimestamp();
+    const newBlock: Block = findBlock(nextIndex, previousBlock.hash, nextTimestamp, blockData, difficulty);
+    addBlock(newBlock);
+    broadcastLatest();
+    return newBlock;
 }
 // ========================================
 
-const calculateHash = (index: number, data: string, timestamp: number, previousHash: string): string => {
-    return CryptoJS.SHA256(index + data + timestamp + previousHash).toString();
+const calculateHash = (index: number, data: string, timestamp: number, previousHash: string, difficulty: number, nonce: number): string => {
+    return CryptoJS.SHA256(index + data + timestamp + previousHash + difficulty + nonce).toString();
 }
 
 const calculateBlockHash = (block: Block): string => {
-    return calculateHash(block.index, block.data, block.timestamp, block.previousHash);
+    return calculateHash(block.index, block.data, block.timestamp, block.previousHash, block.difficulty, block.nonce);
 }
 
 
@@ -82,7 +94,10 @@ const isValidNewBlock = (newBlock: Block, previousBlock: Block): boolean => {
     } else if (previousBlock.hash !== newBlock.previousHash) {
         console.log('invalid previous hash');
         return false;
-    } else if (calculateBlockHash(newBlock) !== newBlock.hash) {
+    } else if (!isValidTimestamp(newBlock, previousBlock)) {
+        console.log('invalid timestamp');
+        return false;
+    } else if (!hasValidHash(newBlock)) {
         console.log('invalid hash');
         return false;
     }
@@ -116,15 +131,106 @@ const isAValidChain = (blockchainToValidate: Block[]): boolean => {
 }
 
 const replaceChain = (newBlocks: Block[]) => {
-    if (isAValidChain(newBlocks) && newBlocks.length > getBlockchain.length) {
+    if (isAValidChain(newBlocks) &&
+        getAccumulatedDifficulty(newBlocks) > getAccumulatedDifficulty(getBlockchain())) {
         console.log('Received blockchain is valid. Replacing current blockchain with received blockchain');
         blockchain = newBlocks;
-
-        // Todo: broadcastLatest(); -- Not explained again....
         broadcastLatest();
     } else {
         console.log('Received blockchain invalid');
     }
 }
 
-export { Block, getBlockchain, getLatestBlock, generateNextBlock, isValidNewBlock, isValidBlockStructure, addBlock, replaceChain };
+// Chapter 2 - Proof of Work
+const hashMatchesDifficulty = (hash: string, difficulty: number): boolean => {
+    const hashInBinary: string = hexToBinary(hash);
+    const requiredPrefix: string = '0'.repeat(difficulty);
+    return hashInBinary.startsWith(requiredPrefix);
+};
+
+// "Mining" function
+const findBlock = (index: number, previousHash: string, timestamp: number, data: string, difficulty: number): Block => {
+    let nonce = 0;
+    while (true) {
+        const hash: string = calculateHash(index, previousHash, timestamp, data, difficulty, nonce);
+        if (hashMatchesDifficulty(hash, difficulty)) {
+            return new Block(index, data, timestamp, hash, previousHash, difficulty, nonce);
+        }
+        nonce++;
+    }
+};
+
+const hashMatchesBlockContent = (block: Block): boolean => {
+    const blockHash: string = calculateBlockHash(block);
+    return blockHash === block.hash;
+};
+
+const hasValidHash = (block: Block): boolean => {
+
+    if (!hashMatchesBlockContent(block)) {
+        console.log('invalid hash, got:' + block.hash);
+        return false;
+    }
+
+    if (!hashMatchesDifficulty(block.hash, block.difficulty)) {
+        console.log('block difficulty not satisfied. Expected: ' + block.difficulty + 'got: ' + block.hash);
+    }
+    return true;
+};
+
+
+// in seconds
+const BLOCK_GENERATION_INTERVAL: number = 10;
+
+// in blocks
+const DIFFICULTY_ADJUSTMENT_INTERVAL: number = 10;
+
+/**
+ * Adjust the difficulty of the blockchain by comparing the time taken to mine the last block with the expected time
+ * @param latestBlock 
+ * @param aBlockchain 
+ * @returns 
+ */
+const getAdjustedDifficulty = (latestBlock: Block, aBlockchain: Block[]) => {
+    const prevAdjustmentBlock: Block = aBlockchain[blockchain.length - DIFFICULTY_ADJUSTMENT_INTERVAL];
+    const timeExpected: number = BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
+    const timeTaken: number = latestBlock.timestamp - prevAdjustmentBlock.timestamp;
+    if (timeTaken < timeExpected / 2) {
+        return prevAdjustmentBlock.difficulty + 1;
+    } else if (timeTaken > timeExpected * 2) {
+        return prevAdjustmentBlock.difficulty - 1;
+    } else {
+        return prevAdjustmentBlock.difficulty;
+    }
+};
+
+/**
+ * Get the current difficulty of the blockchain (number of zeros that the hash must start with on its binary representation)
+ * @param aBlockchain it is obviously the blockchain
+ * @returns 
+ */
+const getDifficulty = (aBlockchain: Block[]): number => {
+    const latestBlock: Block = aBlockchain[blockchain.length - 1];
+    if (latestBlock.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 && latestBlock.index !== 0) {
+        return getAdjustedDifficulty(latestBlock, aBlockchain);
+    } else {
+        return latestBlock.difficulty;
+    }
+};
+
+const getAccumulatedDifficulty = (aBlockchain: Block[]): number => {
+    return aBlockchain
+        .map((block) => block.difficulty)
+        .map((difficulty) => Math.pow(2, difficulty))
+        .reduce((a, b) => a + b);
+};
+
+const getCurrentTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const isValidTimestamp = (newBlock: Block, previousBlock: Block): boolean => {
+    return (previousBlock.timestamp - 60 < newBlock.timestamp)
+        && newBlock.timestamp - 60 < getCurrentTimestamp();
+};
+
+
+export { Block, getBlockchain, getLatestBlock, generateNextBlock, isValidBlockStructure, addBlock, replaceChain };
